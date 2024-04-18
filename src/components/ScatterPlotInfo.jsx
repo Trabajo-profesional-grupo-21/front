1e28c03f05f0cd745e4b59d8ab7a4978d615762a
solfonte <msfontenla@fi.uba.ar>
2024-04-18 19:42:25 -0300

    display label on the scatter plot

--- src/components/ScatterPlotInfo.jsx
+++ src/components/ScatterPlotInfo.jsx
@@ -1,83 +1,112 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Chart from 'chart.js/auto';
-import 'chartjs-plugin-annotation';
 
 export const ScatterPlot = () => {
-    const chartContainer = useRef(null);
-  
+    const chartRef = useRef(null);
+    const [chartVersion, setChartVersion] = useState(null);
+    const chartInstanceRef = useRef(null);
+
     useEffect(() => {
-        const ctx = chartContainer.current.getContext('2d');
-      
-        let data = {
+        const data = {
             datasets: [{
-                label: "Scatter Plot",
+                label: 'Arousal',
                 data: [
-                    { x: -3, y: 2, label: 'sad' },
-                    { x: 1, y: -4, label: 'happy' },
-                    { x: 2, y: 3, label: 'angry' },
+                    { x: -4.5, y: 0.9 }
                 ],
-                backgroundColor: "rgba(255, 99, 132, 0.5)"
+                backgroundColor: [
+                    'rgba(255, 26, 104, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(0, 0, 0, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 26, 104, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(0, 0, 0, 1)'
+                ],
+                borderWidth: 1
             }]
         };
-      
-        let options = {
-            plugins: {
-                legend: {
-                    display: false
-                },
-                tooltip: {
-                    enabled: false
-                },
-                annotation: {
-                    annotations: {
-                        backgroundLabel: {
-                            type: 'box',
-                            xMin: -4,
-                            xMax: -4,
-                            yMin: 0,
-                            yMax: 0,
-                            backgroundColor: 'rgba(255, 255, 255, 0.5)',
-                            borderColor: 'rgba(0, 0, 0, 0.5)',
-                            borderWidth: 1,
-                            label: {
-                                enabled: true,
-                                content: 'Background Label',
-                                position: 'center'
-                            }
-                        }
+
+        const scatterLabels = {
+            id: 'scatterLabels',
+            beforeDatasetsDraw: ((chart, args, plugins) => {
+                const {ctx, data, chartArea: {left, right, top, bottom}} = chart;
+                ctx.save();
+
+                const emotions = [
+                    {
+                        name: 'emotion', 
+                        xPos: left + 5, 
+                        yPos: top + 15, 
+                        textAlign: 'left',
+                    },
+                ]
+                emotions.forEach((item) => {
+                    showEmotion(item.name, item.xPos, item.yPos, item.textAlign)
+                })
+
+                function showEmotion(emotion, positionX, positionY, textAlign) {
+                    ctx.font = '12px sans-serif';
+                    ctx.textAlign = textAlign;
+                    ctx.fillText(emotion, positionX, positionY)
+                }
+            })
+        }
+
+        const config = {
+            type: 'scatter',
+            data: data,
+            options: {
+                scales: {
+                    y: {
+                        position: 'center',
+                        min: -1,
+                        max: 1,
+                    },
+                    x: {
+                        position: 'center',
+                        min: -5,
+                        max: 5,
                     }
                 }
             },
-            scales: {
-                x: {
-                    type: 'linear',
-                    min: -5,
-                    max: 5,
-                    position: 'center'
-                },
-                y: {
-                    type: 'linear',
-                    min: -5,
-                    max: 5,
-                    position: 'center'
-                }
+            plugins: [scatterLabels]
+        };
+
+        if (chartInstanceRef.current) {
+            chartInstanceRef.current.destroy();
+        }
+
+        if (chartRef.current) {
+            chartInstanceRef.current = new Chart(chartRef.current, config);
+        }
+
+        // Set Chart.js version
+        setChartVersion(Chart.version);
+
+        return () => {
+            if (chartInstanceRef.current) {
+                chartInstanceRef.current.destroy();
             }
         };
 
-        const scatterChart = new Chart(ctx, {
-            type: "scatter",
-            data: data,
-            options: options
-        });
-  
-        return () => {
-            scatterChart.destroy();
-        };
     }, []);
-  
+
     return (
-        <div style={{ width: '600px', height: '400px' }}>
-            <canvas ref={chartContainer}></canvas>
+        <div>
+            <div className="chartCard">
+                <div className="chartBox">
+                    <canvas ref={chartRef} id="myChart"></canvas>
+                </div>
+            </div>
         </div>
     );
 };
