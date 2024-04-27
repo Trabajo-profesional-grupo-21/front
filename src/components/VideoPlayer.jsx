dac8c8b6053ae3492ae63b2d65c905c9860f8dd9
solfonte <msfontenla@fi.uba.ar>
2024-04-27 15:37:00 -0300

    use react player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,8 +1,11 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { useCustomWebSocket } from '../components/CustomWebSocketProvider';
+import React, {useState} from 'react';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import ReactPlayer from 'react-player';
 
-export const VideoPlayer = ({ videoFile }) => {
-    const videoRef = useRef(null);
+
+/* 
+
+const videoRef = useRef(null);
     const [currentVideoUrl, setCurrentVideoUrl] = useState(null);
     const [videoDuration, setVideoDuration] = useState(null);
     const [frameRates, setFramesRate] = useState(0);
@@ -94,18 +97,78 @@ export const VideoPlayer = ({ videoFile }) => {
         if (videoRef.current) {
             videoRef.current.load();
         }
-    }, [currentVideoUrl]);
+    }, [currentVideoUrl]);*/
 
+export const VideoPlayer = ({ videoUrl }) => {
+    const socket = useCustomWebSocket();
+    useEffect(() => {
+        if (socket) {
+            if (socket.readyState === WebSocket.OPEN) {
+                if (videoFile) {
+                    const reader = new FileReader();
+                    reader.onload = function (event) {
+                        const videoData = event.target.result;
+                        socket.send(videoData); 
+                    };
+                    reader.readAsArrayBuffer(videoFile);
+                    console.log('Archivo cargado:', videoFile);
+                }
+            } else {
+                socket.onopen = () => {
+                    console.log('Conexión WebSocket establecida, enviando datos...');
+                    if (videoFile) {
+                        const reader = new FileReader();
+                        reader.onload = function (event) {
+                            const videoData = event.target.result;
+                            socket.send(videoData); 
+                        };
+                        reader.readAsArrayBuffer(videoFile);
+                        console.log('Archivo cargado:', videoFile);
+                    }
+                };
+            }
+
+            socket.onmessage = (event) => {
+                
+                console.log('Respuesta recibida de info del video:', event.data);
+                const messageData = JSON.parse(event.data);
+                setFramesRate(messageData.fps);
+                setTotalFrames(messageData.frame_count);
+
+            };
+        }
+    }, [socket]);
+
+    useEffect(() => {
+        if (videoFile) {
+            const url = URL.createObjectURL(videoFile);
+            setCurrentVideoUrl(url);
+        }
+    }, [videoFile]);
+
+    const url = URL.createObjectURL(videoUrl);
+    const handleProgress = (state) => {
+        currentFrame = currentFrame + 1
+        console.log(state)
+        console.log('current frame: ', currentFrame)
+    }
+    var currentFrame = 0;
+    const interval = 1000 /* ms */ / 25 /* fps */
     return (
-        <video
-            ref={videoRef}
-            width="500"
-            height="360"
-            controls
-            onTimeUpdate={handleTimeUpdate}
-        >
-            <source src={currentVideoUrl} type="video/mp4" />
-            Tu navegador no admite la etiqueta de video.
-        </video>
+        <Card sx={{ maxWidth: "100%"}}>
+            <ReactPlayer
+        url={url}
+        controls={true}
+        width="100%"
+        height="100%"
+        onProgress={handleProgress}
+        progressInterval={interval}
+      />
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    Video
+                </Typography>
+            </CardContent>
+        </Card>
     );
 };
