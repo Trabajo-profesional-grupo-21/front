7155a683fe22ee07ad6769e72024400252e6a9f3
agustinaa235 <asegura@fi.uba.ar>
2024-05-25 13:55:29 -0300

    Feat: add emotion labels to russel chart

--- src/charts/ScatterPlotInfo.jsx
+++ src/charts/ScatterPlotInfo.jsx
@@ -22,27 +22,74 @@ export const ScatterPlot = ({xpos, ypos}) => {
                 ctx.save();
                 ctx.fillStyle = plotLabelsColor
 
+                const valenceToCanvasX = val => left + (right - left) * (val + 1) / 2;
+                const arousalToCanvasY = arousal => bottom - (bottom - top) * (arousal + 1) / 2;
+
                 const valueReferences = [
-                    { name: 'Negativo', xPos: left + left*0.10, yPos: (top + bottom) / 2, textAlign: 'left' },
-                    { name: 'Positvo', xPos: right - right*0.10, yPos: (top + bottom) / 2, textAlign: 'left' },
-                    { name: 'Alta', xPos: (left + right) / 2, yPos: top, textAlign: 'left' },
-                    { name: 'Baja', xPos: (left + right) / 2, yPos: bottom, textAlign: 'left' },
+                    { name: '-', xPos: valenceToCanvasX(-1), yPos: arousalToCanvasY(0), textAlign: 'left' },
+                    { name: '+', xPos: valenceToCanvasX(1), yPos: arousalToCanvasY(0), textAlign: 'right' },
+                    { name: '+', xPos: valenceToCanvasX(0), yPos: arousalToCanvasY(0.95), textAlign: 'center' },
+                    { name: '-', xPos: valenceToCanvasX(0), yPos: arousalToCanvasY(-1), textAlign: 'center' },
                 ]
 
-                const emotions = [
-                    { name: 'Enojado', xPos: (left + right) * 0.18, yPos: (top + bottom) * 0.25, textAlign: 'left' },
-                    { name: 'Feliz', xPos: (left + right) * 0.75, yPos: (top + bottom) * 0.25 , textAlign: 'left' },
-                    { name: 'Triste', xPos: (left + right) * 0.18, yPos: (top + bottom) * 0.75, textAlign: 'left' },
-                    { name: 'Relajado', xPos: (left + right) * 0.70, yPos: (top + bottom) * 0.75, textAlign: 'left' }
+                const specificEmotions = [
+                     // Primer cuadrante (positivo, alta activación)
+                    { name: 'asombrado', xPos: valenceToCanvasX(0.2), yPos: arousalToCanvasY(0.9), textAlign: 'center' },
+                    { name: 'Emocionado', xPos: valenceToCanvasX(0.8), yPos: arousalToCanvasY(0.8), textAlign: 'center' },
+                    { name: 'divertido', xPos: valenceToCanvasX(0.4), yPos: arousalToCanvasY(0.65), textAlign: 'center' },
+                    { name: 'Encantado', xPos: valenceToCanvasX(0.6), yPos: arousalToCanvasY(0.3), textAlign: 'center' },
+                    { name: 'Felicidad', xPos: valenceToCanvasX(0.8), yPos: arousalToCanvasY(0.4), textAlign: 'center' },
+                    { name: 'contento', xPos: valenceToCanvasX(0.7), yPos: arousalToCanvasY(0.2), textAlign: 'center' },
+                    { name: 'complacido', xPos: valenceToCanvasX(0.8), yPos: arousalToCanvasY(0.1), textAlign: 'center' },
+
+                    // Segundo cuadrante (negativo, alta activación)
+                    { name: 'alarmado', xPos: valenceToCanvasX(-0.2), yPos: arousalToCanvasY(0.85), textAlign: 'center' },
+                    { name: 'asustado', xPos: valenceToCanvasX(-0.6), yPos: arousalToCanvasY(0.8), textAlign: 'center' },
+                    { name: 'tenso', xPos: valenceToCanvasX(-0.3), yPos: arousalToCanvasY(0.65), textAlign: 'center' },
+                    { name: 'ira', xPos: valenceToCanvasX(-0.8), yPos: arousalToCanvasY(0.7), textAlign: 'center' },
+                    { name: 'frustrado', xPos: valenceToCanvasX(-0.8), yPos: arousalToCanvasY(0.5), textAlign: 'center' },
+                    { name: 'afligido', xPos: valenceToCanvasX(-0.6), yPos: arousalToCanvasY(0.2), textAlign: 'center' },
+                    { name: 'Molesto', xPos: valenceToCanvasX(-0.4), yPos: arousalToCanvasY(0.4), textAlign: 'center' },
+
+                    // Tercer cuadrante (negativo, baja activación)
+                    { name: 'miserable', xPos: valenceToCanvasX(-0.85), yPos: arousalToCanvasY(-0.2), textAlign: 'center' },
+                    { name: 'tristeza', xPos: valenceToCanvasX(-0.7), yPos: arousalToCanvasY(-0.3), textAlign: 'center' },
+                    { name: 'deprimido', xPos: valenceToCanvasX(-0.8), yPos: arousalToCanvasY(-0.4), textAlign: 'center' },
+                    { name: 'sombrío', xPos: valenceToCanvasX(-0.8), yPos: arousalToCanvasY(-0.6), textAlign: 'center' },
+                    { name: 'aburrido', xPos: valenceToCanvasX(-0.7), yPos: arousalToCanvasY(-0.8), textAlign: 'center' },
+                    { name: 'caído', xPos: valenceToCanvasX(-0.2), yPos: arousalToCanvasY(-0.9), textAlign: 'center' },
+
+                    // Cuarto cuadrante (positivo, baja activación)
+                    { name: 'Contento', xPos: valenceToCanvasX(0.8), yPos: arousalToCanvasY(-0.2), textAlign: 'center' },
+                    { name: 'Satisfceho', xPos: valenceToCanvasX(0.85), yPos: arousalToCanvasY(-0.3), textAlign: 'center' },
+                    { name: 'Sereno', xPos: valenceToCanvasX(0.4), yPos: arousalToCanvasY(-0.4), textAlign: 'center' },
+                    { name: 'A gusto ', xPos: valenceToCanvasX(0.6), yPos: arousalToCanvasY(-0.4), textAlign: 'center' },
+                    { name: 'Calmado', xPos: valenceToCanvasX(0.7), yPos: arousalToCanvasY(-0.6), textAlign: 'center' },
+                    { name: 'Relajado', xPos: valenceToCanvasX(0.8), yPos: arousalToCanvasY(-0.8), textAlign: 'center' },
+                    { name: 'somnoliento', xPos: valenceToCanvasX(0.4), yPos: arousalToCanvasY(-0.9), textAlign: 'center' }, 
+                    { name: 'Cansado', xPos: valenceToCanvasX(0.2), yPos: arousalToCanvasY(-0.8), textAlign: 'center' }
+
+                ]
+
+                const QuarterEmotions = [
+                    { name: 'Enojado', xPos: valenceToCanvasX(-0.5), yPos: arousalToCanvasY(0.5), textAlign: 'center' },
+                    { name: 'Feliz', xPos: valenceToCanvasX(0.5), yPos: arousalToCanvasY(0.5), textAlign: 'center' },
+                    { name: 'Triste', xPos: valenceToCanvasX(-0.5), yPos: arousalToCanvasY(-0.5), textAlign: 'center' },
+                    { name: 'Relajado', xPos: valenceToCanvasX(0.5), yPos: arousalToCanvasY(-0.5), textAlign: 'center' }
                 ];
 
                 valueReferences.forEach(item => {
+                    ctx.font = '30px sans-serif';
+                    ctx.textAlign = item.textAlign;
+                    ctx.fillText(item.name, item.xPos, item.yPos);
+                });
+                specificEmotions.forEach(item => {
                     ctx.font = '12px sans-serif';
                     ctx.textAlign = item.textAlign;
                     ctx.fillText(item.name, item.xPos, item.yPos);
                 });
 
-                emotions.forEach(item => {
+                QuarterEmotions.forEach(item => {
                     ctx.font = '20px sans-serif';
                     ctx.textAlign = item.textAlign;
                     ctx.fillText(item.name, item.xPos, item.yPos);
